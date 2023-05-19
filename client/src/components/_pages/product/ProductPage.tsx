2179e9388b535fc62b6f08fecc494f88931c6562
VanyaMate <buhu653@mail.ru>
2023-05-19 21:18:37 +0300

    add useparams to pp

--- client/src/components/_pages/product/ProductPage.tsx
+++ client/src/components/_pages/product/ProductPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
 
 const ProductPage = () => {
+    const { id } = useParams();
+
     return (
         <div>
-            ProductPage
+            ProductPage { id }
         </div>
     );
 };
