d93ea8c8c9f3ecf855f7130c00988f554e48d775
VanyaMate <buhu653@mail.ru>
2023-05-21 21:50:23 +0300

    add constant to path

--- client/src/components/_product/product-card/ProductCard.tsx
+++ client/src/components/_product/product-card/ProductCard.tsx
@@ -5,6 +5,7 @@ import Button from "../../_ui/_buttons/button/Button";
 import ProductPrice from "../product-price/ProductPrice";
 import {Link} from "react-router-dom";
 import DefaultLink from "../../_ui/_links/default-link/DefaultLink";
+import {ROUTE_CATALOGUE, ROUTE_PRODUCT} from "../../../cfg/links.config";
 
 export interface IProductCardData {
     _id: string;
@@ -29,9 +30,9 @@ const ProductCard: React.FC<IProductCardData> = (props) => {
             <ProductCardSlider slides={[props.generalImage, ...(props.images || [])]}/>
             <div className={css.head}>
                 <div className={css.article}>article: 6893890</div>
-                <DefaultLink to={`/catalogue/?brand=${props.brand.title}`} className={css.brand}>{ props.brand.title }</DefaultLink>
+                <DefaultLink to={`${ROUTE_CATALOGUE}/?brand=${props.brand.title}`} className={css.brand}>{ props.brand.title }</DefaultLink>
             </div>
-            <DefaultLink to={`/product/${ props._id }`} className={css.title}>{ props.title }</DefaultLink>
+            <DefaultLink to={`${ROUTE_PRODUCT}/${ props._id }`} className={css.title}>{ props.title }</DefaultLink>
             <ProductPrice {...props} className={css.price}/>
             <Button
                 onClick={() => {}}
