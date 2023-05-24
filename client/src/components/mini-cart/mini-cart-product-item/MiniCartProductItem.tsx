3794ae39cdcb26a44de514b5896b7c5d5e946013
VanyaMate <buhu653@mail.ru>
2023-05-24 05:23:31 +0300

    чуть пофикшен miniCart + добавлен vertical

--- client/src/components/mini-cart/mini-cart-product-item/MiniCartProductItem.tsx
+++ client/src/components/mini-cart/mini-cart-product-item/MiniCartProductItem.tsx
@@ -5,15 +5,16 @@ import Row from "../../_ui/_containers/row/Row";
 import AnimatedImageContainer from "../../_ui/_images/animated-image-container/AnimatedImageContainer";
 import ProductPrice from "../../_product/product-price/ProductPrice";
 import {BACKEND_HOST} from "../../../cfg/links.config";
+import Vertical from "../../_ui/_containers/vertical/Vertical";
 
 const MiniCartProductItem: React.FC<ICartItem> = (props) => {
     return (
         <Row className={css.container} offset={10}>
             <AnimatedImageContainer src={BACKEND_HOST + '/' + props.product.generalImage} w={60} h={60} seconds={5} className={css.image}/>
-            <div>
-                <div>{ props.product.title }</div>
+            <Vertical offset={5}>
+                <div>{ props.product.title } [ {props.amount} ]</div>
                 <ProductPrice {...props.product}/>
-            </div>
+            </Vertical>
             <div>Controll</div>
         </Row>
     );
