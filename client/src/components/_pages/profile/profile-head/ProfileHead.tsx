ccd0109bfcac0275f10502d3b1d49cba84ff9bfc
VanyaMate <buhu653@mail.ru>
2023-05-28 11:19:09 +0300

    add minicart to profile page

--- client/src/components/_pages/profile/profile-head/ProfileHead.tsx
+++ client/src/components/_pages/profile/profile-head/ProfileHead.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import {IUser} from "../../../../store/auth/auth.types";
 import AnimatedImageContainer from "../../../_ui/_images/animated-image-container/AnimatedImageContainer";
 import Row from "../../../_ui/_containers/row/Row";
+import MiniCart from "../../../_cart/mini-cart/MiniCart";
+import css from './ProfileHead.module.scss';
+import TitledBlock from "../../../titled-block/TitledBlock";
 
 const ProfileHead: React.FC<IUser> = (props) => {
     return (
-        <Row offset={10}>
-            <AnimatedImageContainer src={props.avatar ?? ''} w={300} h={300} seconds={20}/>
-            <h1>{ props.email }</h1>
-        </Row>
+        <div className={css.container}>
+            <Row offset={10}>
+                <AnimatedImageContainer src={props.avatar ?? ''} w={300} h={300} seconds={20}/>
+                <h1>{ props.email }</h1>
+            </Row>
+            <TitledBlock title={'Корзина'}>
+                <MiniCart/>
+            </TitledBlock>
+        </div>
     );
 };
 
