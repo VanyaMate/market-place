7d6a69e23a8fd934d3c73addc1f6a10d2932678e
VanyaMate <buhu653@mail.ru>
2023-06-06 21:04:42 +0300

    update bus company page

--- client/src/components/_pages/business/BusinessCompanyPage.tsx
+++ client/src/components/_pages/business/BusinessCompanyPage.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import {useParams} from "react-router-dom";
 import {useGetFullDataCompanyQuery} from "../../../store/companies/companies.api";
 import CompanyItem from "../../_companies/company-item/CompanyItem";
+import TitledBlock from "../../titled-block/TitledBlock";
+import Vertical from "../../_ui/_containers/vertical/Vertical";
 
 const BusinessCompanyPage = () => {
     const params = useParams<{ companyTitle: string }>();
     const { isFetching, isError, data } = useGetFullDataCompanyQuery({ title: params.companyTitle! });
 
     return (
-        <div>
+        <Vertical offset={20}>
             { data ? <CompanyItem company={data}/> : '' }
-        </div>
+            <TitledBlock title={'Бренды'}>
+
+            </TitledBlock>
+        </Vertical>
     );
 };
 
