7c639a7ffa307a28a9ed3471c7c900dc7d4d5778
VanyaMate <buhu653@mail.ru>
2023-06-08 20:17:08 +0300

    final?

--- server/src/api/v2/user/user.service.ts
+++ server/src/api/v2/user/user.service.ts
@@ -1,11 +1,10 @@
 import {Injectable} from "@nestjs/common";
 import {Service} from "../service-middleware.interface";
-import {UserServiceSequelize} from "../service-middlewares/user/user.service-sequelize";
 import {UserServiceMongoose} from "../service-middlewares/user/user.service-mongoose";
 
 @Injectable()
 export class UserService extends Service<User, UserDto> {
-    constructor(private userService: UserServiceMongoose) { super(userService) }
+    constructor(userService: UserServiceMongoose) { super(userService) }
 
     async create (name: string): Promise<User> {
         return await this.middleware.create({ name })
